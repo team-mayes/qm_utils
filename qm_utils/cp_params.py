Fixed calculation of closest pucker

qm_utils/cp_params.py
@@ -4,9 +4,9 @@
 """
 From 2007Hill_Supporting.pdf
 
-cp.py
+cp_params.py
 Usage:
-./cp.py file.dat
+./cp_params.py file.dat
 This is a script that reads a list of atomic coordinates
 for several 6-member rings and outputs Cremer-Pople
 puckering coordinates.
@@ -32,7 +32,7 @@ import numpy as np
 import argparse
 import sys
 import math
-from qm_common import GOOD_RET, INVALID_DATA, warning, read_csv_to_dict, create_out_fname, write_csv
+from qm_common import GOOD_RET, INVALID_DATA, warning, read_csv_to_dict, create_out_fname, write_csv, InvalidDataError
 
 __author__ = 'hbmayes'
 
@@ -48,6 +48,10 @@ THETA_KEY = 'theta'
 Q_KEY = 'q'
 PUCKER_KEY = 'closest_pucker'
 OUT_KEYS = [NAME_KEY, PHI_KEY, THETA_KEY, Q_KEY, PUCKER_KEY]
+
+PHI_RAD = 'phi_rad'
+THETA_RAD = 'theta_rad'
+XYZ = 'cart_xyz'
 
 
 def parse_cmdline(argv):
@@ -85,6 +89,7 @@ def parse_cmdline(argv):
         args = parser.parse_args(argv)
     except SystemExit as e:
         warning(e)
+        parser.print_help()
         return [], INVALID_DATA
 
     return args, GOOD_RET
@@ -97,18 +102,42 @@ def to_float_list(a):
     return b
 
 
-def find_closest_pucker(phi1, theta1, pucker_dict):
+def angles_to_xyz(phi, theta):
+    """
+    Calculates the distance on a unit sphere for Cremer-Pople coordinates
+    Note!!! CP uses theta for latitude!! this is opposite of much of math convention
+    :param phi: longitude in radians
+    :param theta: latitude in radians
+    :return: Cartesian coordinates along the unit sphere, as a numpy array
+    """
+    sin_theta = math.sin(theta)
+    return np.array((sin_theta * math.cos(phi),
+                     sin_theta * math.sin(phi),
+                     math.cos(theta)))
+
+
+def find_closest_pucker(phi, theta, pucker_dict):
+    """
+    Calculated based on cord length:
+    delta_x = sin(phi2) * cos(theta2) - cos(ph1)*cos(theta1)
+    delta_y = sin(phi2) * sin(theta2) - cos(phi1)*sin(theta1)
+    delta_z = cos(phi2) - cos(phi1)
+    chord_length = sqrt( delta_x^2 + delta_y^2 + delta_z^2)
+       (aka norm of the vectors)
+    central angle (aka delta_sigma) = 2 * arcsin( chord / 2)
+    d = r * delta_sigma
+    :param phi: phi of the pucker to identify
+    :param theta: second CP parameter of the pucker to identify
+    :param pucker_dict: dictionary of names and CP params of IUPAC puckers
+    :return: closest IUPAC pucker
+    """
     closest_pucker = 'nan'
     closest_dist = np.inf
-    sin_phi1 = math.sin(phi1)
-    cos_phi1 = math.cos(phi1)
+    xyz = angles_to_xyz(phi, theta)
 
     for pucker in pucker_dict:
-        sin_phi2 = math.sin(pucker_dict[pucker][0])
-        cos_phi2 = math.cos(pucker_dict[pucker][0])
-        theta2 = pucker_dict[pucker][1]
-        cos_angle = (sin_phi1 * sin_phi2 * math.cos(theta1 - theta2) + cos_phi1 * cos_phi2)
-        current_dist = math.acos(cos_angle)
+        chord = np.linalg.norm(xyz-pucker_dict[pucker][XYZ])
+        current_dist = 2. * math.asin(chord / 2.)
         if current_dist < closest_dist:
             closest_dist = current_dist
             closest_pucker = pucker
@@ -130,6 +159,10 @@ def process_file(input_file, pucker_dict):
 
         for line in f:
             s_line = line.split()
+            if len(s_line) != 19:
+                raise InvalidDataError("Expected exactly 19 space-separated entries per line: a header "
+                                       "followed by 18 float values. \n"
+                                       "However, found {} on line: {}".format(len(s_line), line))
             header = s_line[0]
             for i in range(1, 19, 3):
                 atoms[(i - 1) / 3] = to_float_list(s_line[i:i + 3])
@@ -192,7 +225,7 @@ def process_file(input_file, pucker_dict):
                     # bigQ2 = np.array([q1,q2,q3],dtype='float64')
             # bigQ2 = math.sqrt((bigQ2*bigQ2).sum())
             closest_pucker = find_closest_pucker(np.deg2rad(phi), np.deg2rad(theta), pucker_dict)
-
+            print("{:12} {:8.3f} {:8.3f} {:8.3f} {}".format(header, phi, theta, big_q, closest_pucker))
             pucker_results.append({NAME_KEY: header, PHI_KEY: phi, THETA_KEY: theta, Q_KEY: big_q,
                                    PUCKER_KEY: closest_pucker})
 
@@ -210,7 +243,12 @@ def create_pucker_dict(pucker_dict_file):
 
     rad_dict = {}
     for line in raw_dict:
-        rad_dict[line["pucker_id"]] = [np.deg2rad(line["phi,degrees"]), np.deg2rad(line["theta,degrees"])]
+        phi = np.deg2rad(line["phi,degrees"])
+        theta = np.deg2rad(line["theta,degrees"])
+        rad_dict[line["pucker_id"]] = {PHI_RAD: phi,
+                                       THETA_RAD: theta,
+                                       XYZ: angles_to_xyz(phi, theta),
+                                       }
     return rad_dict
 
 
@@ -230,8 +268,9 @@ def main(argv=None):
         if args.out_file is None:
             args.out_file = create_out_fname(args.file, base_dir=args.out_dir, ext='.out')
         write_csv(result_dict, args.out_file, OUT_KEYS)
-    finally:
-        pass
+    except InvalidDataError as e:
+        warning(e)
+        return INVALID_DATA
 
     return GOOD_RET  # success
 