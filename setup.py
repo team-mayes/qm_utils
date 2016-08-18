6c4fd07f8eb09cc492b0c5161c958307bd6d7897
Heather Mayes <hmayes@hmayes.com>
2016-08-18 17:32:58 -0500

    Updated program name in setup.py

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
                  'qm_utils'},
     entry_points={
         'console_scripts': ['cp_params=qm_utils.cp_params:main',
-                            'read_pdb=qm_utils.read_pdb:main',
-                            'read_sdf=qm_utils.read_sdf:main',
+                            'coord_to_com=qm_utils.coord_to_com:main',
                             ]
     },
     include_package_data=True,
