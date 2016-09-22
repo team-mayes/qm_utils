cf9b8f4a1d02a9c4f84733eda7bbd47f0aca12ae
Vicchio-7 <vicchio@umich.edu>
2016-09-22 13:55:00 -0400

    working on the xyz_cluster.py

--- qm_utils/xyz_cluster.py
+++ qm_utils/xyz_cluster.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """
 (insert a description about the code here)
@@ -9,18 +8,19 @@ can be clustered.
 
 """
 
-from sys import argv
-
-__author__ = 'SPVicchio'
-
 # Libraries
+from __future__ import print_function
 from sys import argv
 from qm_common import find_files_by_dir
 import numpy as np
 import re
 
+__author__ = 'SPVicchio'
+
+
 # Constants #
 
+TOL_centroid = [0.001, 0.001, 0.001]
 
 # Defaults #
 
@@ -55,7 +55,7 @@ def get_coordinates_xyz(filename):
     # Skip the title line
     f.next()
 
-    xyz_coords = np.full((num_atoms, 3), np.nan) # creates an array that will be populated later with xyz coordinates
+    xyz_coords = np.full((num_atoms, 3), np.nan)  # creates an array that will be populated later with xyz coordinates
     for line in f:
         if lines_read == num_atoms:
             break
@@ -72,10 +72,18 @@ def get_coordinates_xyz(filename):
 
     return num_atoms, xyz_atoms, xyz_coords
 
+
 def print_xyz_coord_info(num_atoms, xyz_atoms, xyz_coords):
-    print "Your molecule currently has %d atoms." % num_atoms
-    print "\nThe atom ordering is:\n %r" % xyz_atoms
-    print "\nThe XYZ coordinates are:\n %r" % xyz_coords
+    """ Prints the information from the get_coordinates_xyz
+    :param num_atoms: the number of atoms in the molecule
+    :param xyz_atoms: prints the order of the atoms (used to know which coordinates are which in xyz_cords)
+    :param xyz_coords: prints the xyz coordinates (the row correspondence can be found from xyz_atoms)
+    :return:
+    """
+    print("Your molecule currently has {} atoms.".format(num_atoms))
+    print("\nThe atom ordering is:\n {}".format(xyz_atoms))
+    print("\nThe XYZ coordinates are:\n {}".format(xyz_coords))
+
 
 def centroid(X):
     """ Calculates the centroid (geometric center) of the structure. The centroid
@@ -84,8 +92,28 @@ def centroid(X):
     :param X: xyz coordinates for the given molecular structure
     :return: the xyz coordinates for the centroid of the structure
     """
-    centroid_xyz = sum(X)/len(X)
+    centroid_xyz = sum(X) / len(X)
     return centroid_xyz
+
+
+def translate_centroid_all(xyz_coords):
+    """ Calculates the centroid of the XYZ coordinates based on all the atoms. Once the cetroid is found, then the
+    xyz corodinates are translated so that the centroid is at the origin.
+
+    :param xyz_coords: the xyz coordinates for the molecular structure (centroid not aligned)
+    :return: outputs the xyz coordiantes for the molecular structure with the centroid at zero
+    """
+
+    centroid_xyz = sum(xyz_coords) / len(xyz_coords)
+
+    xyz_coords_translate = [xyz_coords - centroid_xyz]
+
+    centroid_xyz_check = sum(xyz_coords_translate) / len(xyz_coords_translate)
+
+    if centroid_xyz_check.all > TOL_centroid:
+        exit("\nCould not properly align the centroid to the origin\n.")
+
+    return xyz_coords_translate
 
 
 script, input_file1, input_file2 = argv
@@ -94,14 +122,19 @@ n_atoms1, atoms1, xyz_coords1 = get_coordinates_xyz(input_file1)
 n_atoms2, atoms2, xyz_coords2 = get_coordinates_xyz(input_file2)
 
 if n_atoms1 != n_atoms2:
-    print "Error in the number of atoms"
+    print("Error in the number of atoms")
 
-c1 = centroid(xyz_coords1)
-c2 = centroid(xyz_coords2)
+# c1 = centroid(xyz_coords1)
+# c2 = centroid(xyz_coords2)
 
-print c1, c2
+# print("{},{}".format(c1, c2))
 
-print "This is the translated verison\n",[xyz_coords1 - c1]
-print "\n This is the untranslated verison\n", xyz_coords1
+# print("This is the translated verison\n {}".format([xyz_coords1 - c1]))
+# print("\n This is the untranslated verison\n {}".format(xyz_coords1))
 
-print centroid(xyz_coords1 - c1)
+# print("{}".format(centroid(xyz_coords1 - c1)))
+
+
+A = translate_centroid_all(xyz_coords1)
+
+print("{}".format(translate_centroid_all(xyz_coords1)))
