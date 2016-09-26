4679b752db08bbdeb2a550bd8fc53667682538c6
Vicchio-7 <vicchio@umich.edu>
2016-09-26 15:43:13 -0400

    ready for Heather!

--- TODO.rst
+++ TODO.rst
@@ -2,7 +2,15 @@
 qm_utils
 ===============================
 
+xyz_cluster
+------------
+
+Add cluster algorithm
+Add method to read all files in a given directory instead of having to input two specific files
+
 coord_to_com
 ------------
 
 Allow template instead of ini file?
+
+
--- qm_utils/xyz_cluster.py
+++ qm_utils/xyz_cluster.py
@@ -45,6 +45,7 @@ def get_coordinates_xyz(filename):
     f = open(filename, 'r')
 
     xyz_atoms = []
+    atoms_ring_order = []
     num_atoms = 0
     lines_read = 0
     lines_read_ring = 0
@@ -71,6 +72,7 @@ def get_coordinates_xyz(filename):
 
         if atom != '1':
             xyz_coords_ring[lines_read_ring] = numbers
+            atoms_ring_order.append(atom)
             lines_read_ring += 1
 
         lines_read += 1
@@ -78,7 +80,7 @@ def get_coordinates_xyz(filename):
 
     xyz_atoms = np.array(xyz_atoms)
 
-    return num_atoms, xyz_atoms, xyz_coords, xyz_coords_ring
+    return num_atoms, xyz_atoms, xyz_coords, atoms_ring_order, xyz_coords_ring
 
 def print_xyz_coord_info(num_atoms, xyz_atoms, xyz_coords):
     """ Prints the information from the get_coordinates_xyz
@@ -121,30 +123,26 @@ def translate_centroid_all(xyz_coords):
     xyz_coords_translate = np.array(np.mat(xyz_coords_translate))
     return xyz_coords_translate
 
-def translate_centroid_ring(xyz_coords,xyz_atoms):
+def translate_centroid_ring(xyz_coords,xyz_coords_ring):
     """This script is designed to calculate the centroid of the xyz coordinates based on only the atoms in the ring.
     Once the centroid of the ring is found, then the all xyz coordinates are translated so that the centroid of the ring
     is at the origin.
 
     :param xyz_coords: the xyz coordinates for the molecular structure (centroid not aligned)
-    :param xyz_atoms: the atoms of the ring
+    :param xyz_coords_ring: the xyz coordinates for the ring of the molecular structure
     :return: outputs the xyz coordinates for the molecular structure with the ring centroid located at the origin.
     """
 
-    #for
+    centroid_ring_xyz = sum(xyz_coords_ring) / len(xyz_coords_ring)
 
-    # for len(xyz_coords)
+    xyz_coords_all_translate = np.array([xyz_coords - centroid_ring_xyz])
+    xyz_coords_ring_translate = np.array([xyz_coords_ring - centroid_ring_xyz])
 
-    centroid_xyz = sum(xyz_coords) / len(xyz_coords)
 
-    xyz_coords_translate = [xyz_coords - centroid_xyz]
+    xyz_coords_all_translate = np.array(np.mat(xyz_coords_all_translate))
+    xyz_coords_ring_translate = np.array(np.mat(xyz_coords_ring_translate))
 
-    centroid_xyz_check = sum(xyz_coords_translate) / len(xyz_coords_translate)
-
-    if centroid_xyz_check.all > TOL_centroid:
-        exit("\nCould not properly align the centroid to the origin\n.")
-
-    return xyz_coords_translate
+    return xyz_coords_all_translate, xyz_coords_ring_translate
 
 def rmsd(V, W):
     """Calculates the root-mean-square deviation from two sets of vectors xyz_1 and xyz_2 (both of which are xyz
@@ -187,55 +185,111 @@ def kabsch_algorithm(xyz_coords1, xyz_coords2):
 
     kabsch_rsmd = rmsd(rotated_xyz_coords1,xyz_coords2)
 
-
-
-
     return kabsch_rsmd
+
+def check_ring_ordering(atoms_ring_order1, atoms_ring_order2):
+    """ This script ensure that the atom arrangement is the same. Arranging the atoms differently between two xyz files
+    could result in differences between in the calculations. Must compare the oxygen atoms to the oxygen atoms and the
+    carbon atoms to the carbon atoms.
+
+    :param atoms_ring_order1: input a list (in order) of the atoms of the ring
+    :param atoms_ring_order2: input a list (in order) of the atoms of the ring
+    :return: message saying the status of the ordering
+    """
+
+    if atoms_ring_order1 != atoms_ring_order2:
+        exit("The atoms in the ring are not aligned the same!")
+    else:
+        print("The atoms are aligned properly!")
+    return
+
+def print_xyz_coords(to_print_xyz_coords, to_print_atoms, file_sum):
+    """
+
+    :param to_print_xyz_coords:
+    :param to_print_atoms:
+    :return:
+    """
+
+    num_atoms = len(to_print_atoms)
+    num_atoms = '16'
+
+    to_print = [num_atoms, file_sum]
+    to_print2 = list(to_print)
+
+#    for line_id in range(len(atoms1)):
+#        to_print.append([atoms1[line_id]] + xyz_coords1[line_id].tolist())
+
+    for atom_type, atom_xyz in zip(atoms1, xyz_coords1):
+        to_print2.append([atom_type] + atom_xyz.tolist())
+
+    list_to_file(to_print2, file_sum)
+
+    return
+
 
 
 
 ##### Loading Files
 script, input_file1, input_file2 = argv
 
-n_atoms1, atoms1, xyz_coords1, xyz_coords_ring1 = get_coordinates_xyz(input_file1)
-n_atoms2, atoms2, xyz_coords2, xyz_coords_ring2 = get_coordinates_xyz(input_file2)
+n_atoms1, atoms1, xyz_coords1, atoms_ring_order1, xyz_coords_ring1 = get_coordinates_xyz(input_file1)
+n_atoms2, atoms2, xyz_coords2, atoms_ring_order2, xyz_coords_ring2 = get_coordinates_xyz(input_file2)
 
-to_print = ['18', 'testing']
-to_print2 = list(to_print)
-
-
-
-
-for line_id in range(len(atoms1)):
-    print("line_id", line_id)
-    to_print.append([atoms1[line_id]] + xyz_coords1[line_id].tolist())
-
-
-for atom_type, atom_xyz in zip(atoms1, xyz_coords1):
-    to_print2.append([atom_type] + atom_xyz.tolist())
-
-list_to_file(to_print2, 'test3.txt')
 
 if n_atoms1 != n_atoms2:
     exit("Error in the number of atoms! The number of atoms doesn't match!")
+
+check_ring_ordering(atoms_ring_order1,atoms_ring_order2)
+
 #####
 
 
 center_xyz1 = translate_centroid_all(xyz_coords1)
 center_xyz2 = translate_centroid_all(xyz_coords2)
 
+[center_ring_all_xyz1, center_ring_ring_xyz1] = translate_centroid_ring(xyz_coords1,xyz_coords_ring1)
+[center_ring_all_xyz2, center_ring_ring_xyz2] = translate_centroid_ring(xyz_coords2,xyz_coords_ring2)
 
+print_xyz_coords(center_xyz1, atoms1, 'xyz_coords_all-align_1e.xyz')
+print_xyz_coords(center_ring_all_xyz1, atoms1, 'xyz_coords_ring-align_1e.xyz')
+
+print_xyz_coords(center_xyz2, atoms2, 'xyz_coords_all-align_1c4.xyz')
+print_xyz_coords(center_ring_all_xyz2, atoms2, 'xyz_coords_ring-align_1c4.xyz')
 
 
 print("\n The rmsd without aligning and rotating the structures is {}\n".format(rmsd(center_xyz1,center_xyz2)))
 
 print("\n The rmsd from the Kabsch method is: {}\n".format(kabsch_algorithm(center_xyz1,xyz_coords2)))
 
+print("\n\n\n")
+
+print("""Now print the different cases:
+    Rmsd (all align, standard): {}
+    Rmsd (ring align, standard): {}
+    Rmsd (all align, kabsch): {}
+    Rmsd (ring align, kabsch): {}
+    """.format(rmsd(center_xyz1,center_xyz2),rmsd(center_ring_ring_xyz1,center_ring_ring_xyz2),kabsch_algorithm(center_xyz1,center_xyz2),kabsch_algorithm(center_ring_ring_xyz1,center_ring_ring_xyz2)))
 
 
-list_to_file(['18'], 'test_me.txt')
-list_to_file(['testing'], 'test_me.txt', mode='a')
-list_to_file(rotated_xyz_coords1, 'test_me.txt', mode='a')
+
+#to_print = ['18', 'testing']
+#to_print2 = list(to_print)
+
+#for line_id in range(len(atoms1)):
+#    print("line_id", line_id)
+#    to_print.append([atoms1[line_id]] + xyz_coords1[line_id].tolist())
+
+
+#for atom_type, atom_xyz in zip(atoms1, xyz_coords1):
+#    to_print2.append([atom_type] + atom_xyz.tolist())
+
+#list_to_file(to_print2, 'test3.txt')
+
+
+#list_to_file(['18'], 'test_me.txt')
+#list_to_file(['testing'], 'test_me.txt', mode='a')
+#list_to_file(rotated_xyz_coords1, 'test_me.txt', mode='a')
 
 
 #print("{},\n\n{}".format(center_xyz1,center_xyz2))
