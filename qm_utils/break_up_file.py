dd829d68610c0a575fbd5ef97f94a34383ba34f9
Samantha Schwartz <scschwa@umich.edu>
2017-02-08 13:22:13 -0500

    hi

--- qm_utils/break_up_file.py
+++ qm_utils/break_up_file.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 from collections import defaultdict
 
 import pandas as pd
-
+import os
 from qm_utils.hartree_valid import verify_local_minimum, verify_transition_state
 from qm_utils.qm_common import warning, create_out_fname, InvalidDataError, write_csv
 
@@ -28,7 +28,7 @@ def parse_cmdline(argv):
     parser = argparse.ArgumentParser(description='INSERT DESCRIPTION')
 
     parser.add_argument('-i', "--in_file", help='Input file')
-    parser.add_argument('-o', "--out_dir", help='Input file')
+    parser.add_argument('-d', "--out_directory", help='Output directory')
 
     # parser.add_argument("-i", "--input_rates", help="The location of the input rates file",
     #                     default=DEF_IRATE_FILE, type=read_input_rates)
@@ -55,33 +55,33 @@ def main(argv=None):
 
     if ret != 0:
         return ret
-    sorting = True
     hold_lines = []
     with open(args.in_file, 'r') as text_file:
         for row in text_file:
             hold_lines.append(row)
-    sorting = True
-    int i =0
-    if line in ['\n', '\r\n']:
-    while sorting:
-        file_name =  hold_lines[i] + ".txt"
-        hold_new_lines = []
-        for lines in
-        if left < 300:
-            while count < left:
-                hold_new_lines.append(hold_lines[line_count])
-                count += 1
-                line_count += 1
-            sorting = False
-        else:
-            while count < 300:
-                hold_new_lines.append(hold_lines[line_count])
-                count += 1
-                line_count += 1
-        outer_count += 1
-        with open(file_name, 'w') as next_file:
-            for row in hold_new_lines:
-                next_file.write(row)
+    i =0
+    last = 0
+    new_array = []
+    hold_new_lines = []
+    file_names = []
+
+
+    for lines in hold_lines:
+        if lines in ['\n', '\r\n']:
+            new_array = hold_lines[last:i];
+            new_array.insert(0, str(i-last-1)+'\n')
+            hold_new_lines.append(new_array)
+            string_name = hold_lines[last][:(len(hold_lines[last])-1)] + ".xyz"
+            string_name = string_name.replace(" ","")
+            file_names.append(string_name)
+            last = i+1;
+        i = i+1
+
+    for i in range(len(hold_new_lines)):
+        complete_path = os.path.join(args.out_directory, file_names[i])
+        with open(complete_path, 'w') as next_file:
+            for row in hold_new_lines[i]:
+                next_file.write(str(row))
 
     return 0  # success
 
