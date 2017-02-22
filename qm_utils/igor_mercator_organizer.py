0ecefc11af6de4ce74de9df4a86faff47781798d
Vicchio-7 <vicchio@umich.edu>
2017-02-22 11:00:36 -0500

    sending

--- qm_utils/igor_mercator_organizer.py
+++ qm_utils/igor_mercator_organizer.py
@@ -72,7 +72,7 @@ def reading_all_csv_input_files(file_read, molecule):
         elif filename_split[len_file-4] == 'sorted':
             sort_status = filename_split[len_file-4]
         elif filename_split[len_file-4] == 'allunsorted':
-            sort_status = filename_split[len_file-4]
+            sort_status = 'unsorted'
         else:
             print('Some is wrong... not sure whether the file is sorted or not')
 
