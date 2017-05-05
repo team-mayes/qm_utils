42f5453c94e7954149ab67acccf440101e2f58a7
Vicchio-7 <vicchio@umich.edu>
2017-05-05 11:57:51 -0400

    working on the TS part of the analysis

--- qm_utils/spherical_kmeans_voronoi.py
+++ qm_utils/spherical_kmeans_voronoi.py
@@ -9,7 +9,6 @@ for HSP. The output will serve as the foundation for making all comparisons acro
 
 from __future__ import print_function
 
-
 import os
 import sys
 
@@ -19,12 +18,13 @@ from qm_utils.qm_common import read_csv_to_dict, create_out_fname
 from spherecluster import SphericalKMeans
 from scipy.spatial import SphericalVoronoi
 import statistics as st
+import math
+from collections import OrderedDict
 
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from matplotlib import colors
 import matplotlib.pyplot as plt
-
 
 try:
     # noinspection PyCompatibility
@@ -35,8 +35,11 @@ except ImportError:
 
 __author__ = 'SPVicchio'
 
-# Constants #
-
+# # Default Parameters # #
+TOL_ARC_LENGTH = 0.1
+TOL_ARC_LENGTH_CROSS = 0.2
+DEFAULT_TEMPERATURE = 298.15
+K_B = 0.001985877534  # Boltzmann Constant in kcal/mol K
 
 
 # Hartree field headers
@@ -168,28 +171,205 @@ def spherical_kmeans_voronoi(number_clusters, data_points, phi_raw, theta_raw, e
         phi_vertices.append(round(phi_new, 1))
         theta_vertices.append(round(theta_new, 1))
 
-    ind_dict['phi_sv_centers'] = phi_vertices
-    ind_dict['theta_sv_centers'] = theta_vertices
+    ind_dict['phi_sv_vertices'] = phi_vertices
+    ind_dict['theta_sv_vertices'] = theta_vertices
     ind_dict['vertices_sv_xyz'] = sv.vertices
-
-    # HI JUSTIN
 
     return ind_dict
 
 
-def matplotlib_printing_normal(data_dict, dir, save_status='no'):
+def organizing_information_from_spherical_kmeans(data_dict):
+    """
+    This script is designed to organize all of the raw data information into the correct group
+    :param data_dict: the data dict that is output from spherical k-means and voronoi script
+    :return: an organized data dict
+    """
 
+    temp_groups = {}
+    groups = {}
+
+    for g in range(0, data_dict['number_clusters']):
+        temp_dict = {}
+        temp_dict['assignment_key'] = g
+        temp_groups[str('temp_group_' + str(g))] = temp_dict
+
+    for key, key_value in temp_groups.items():
+        temp_dict = {}
+        energies = []
+        theta = []
+        phi = []
+        mean_phi = None
+        mean_theta = None
+        for i in range(0, len(data_dict['labels_skm_centers'])):
+            if key_value['assignment_key'] == data_dict['labels_skm_centers'][i]:
+                energies.append(data_dict['energy'][i])
+                theta.append(data_dict['theta_raw'][i])
+                phi.append(data_dict['phi_raw'][i])
+                mean_phi = data_dict['phi_skm_centers'][key_value['assignment_key']]
+                mean_theta = data_dict['theta_skm_centers'][key_value['assignment_key']]
+
+        boltzman_weighted_energies = boltzmann_weighting_mini(energies)
+
+        temp_dict['energies'] = energies
+        temp_dict['mean_phi'] = str(mean_phi)
+        temp_dict['mean_theta'] = str(mean_theta)
+        temp_dict['phi'] = phi
+        temp_dict['theta'] = theta
+        temp_dict['weighted_gibbs'] = boltzman_weighted_energies
+
+        groups[key] = temp_dict
+
+    final_groups = correcting_group_order(groups)
+
+    return final_groups
+
+
+def correcting_group_order(groups):
+    """
+    Organizes in a logical manner
+    :param groups: the dict with all of the necessary information
+    :return: the final dict the groups properly in order
+    """
+    final_dict = {}
+
+    phi_groups = []
+    theta_groups = []
+
+    groups_top = {}
+    dict_top = {}
+    groups_mid = {}
+    dict_mid = {}
+    groups_bot = {}
+    dict_bot = {}
+
+    for key, key_val in groups.items():
+        phi_groups.append(key_val['mean_phi'])
+        theta_groups.append(key_val['mean_theta'])
+
+    for i in range(0, len(phi_groups)):
+        if float(theta_groups[i]) < 60:
+            for old_id, val in groups.items():
+                if theta_groups[i] == val['mean_theta']:
+                    groups_top[old_id] = val
+                    dict_top[str(val['mean_theta'])] = float(val['mean_phi'])
+                    break
+        elif float(theta_groups[i]) > 120:
+            for old_id, val in groups.items():
+                if theta_groups[i] == val['mean_theta']:
+                    groups_bot[old_id] = val
+                    dict_bot[str(val['mean_theta'])] = float(val['mean_phi'])
+                    break
+        else:
+            for old_id, val in groups.items():
+                if theta_groups[i] == val['mean_theta']:
+                    groups_mid[old_id] = val
+                    dict_mid[str(val['mean_theta'])] = float(val['mean_phi'])
+                    break
+
+    group_count = 0
+
+    order_top = sorted(dict_top, key=dict_top.get, reverse=False)
+    order_mid = sorted(dict_mid, key=dict_mid.get, reverse=False)
+    order_bot = sorted(dict_bot, key=dict_bot.get, reverse=False)
+
+    for row in order_top:
+        for key, key_val in groups.items():
+            if row == key_val['mean_theta']:
+                final_dict['group_' + str(group_count).rjust(2, '0')] = key_val
+                group_count += 1
+
+    for row in order_mid:
+        for key, key_val in groups.items():
+            if row == key_val['mean_theta']:
+                final_dict['group_' + str(group_count).rjust(2, '0')] = key_val
+                group_count += 1
+
+    for row in order_bot:
+        for key, key_val in groups.items():
+            if row == key_val['mean_theta']:
+                final_dict['group_' + str(group_count).rjust(2, '0')] = key_val
+                group_count += 1
+
+    return final_dict
+
+
+def boltzmann_weighting_mini(energies):
+    """
+    Performs boltzmann weighting on the groups
+    :param energies: the relative gibbs free energies
+    :return: the weighted boltzmann parameter
+    """
+    e_val_list = []
+    ind_boltz = []
+    total_botlz = 0
+    for e_val in energies:
+        component = math.exp(-float(e_val) / (K_B * DEFAULT_TEMPERATURE))
+        ind_boltz.append(component)
+        total_botlz += component
+
+    weighted_gibbs_free_energy = 0
+    for i in range(0, len(energies)):
+        weighted_gibbs_free_energy += (ind_boltz[i] / total_botlz) * energies[i]
+
+    return round(weighted_gibbs_free_energy,3)
+
+
+def read_csv_data_TS(filename, dir_):
+    """
+    Reads the CSV file with the information for the TS structures
+    :param filename: the filename of the CSV file
+    :param dir_: the directory
+    :return: data points (np array containing the x, y, z coordinates), phi_raw (phi coords), theta_raw (theta coords)
+    """
+
+    file_sample_ts = os.path.join(dir_, filename)
+    data_dict_ts = read_csv_to_dict(file_sample_ts, mode='r')
+
+    data_points = []
+    phi_raw = []
+    theta_raw = []
+    energy = []
+
+    for i in range(0, len(data_dict_ts)):
+        info = data_dict_ts[i]
+        phi = float(info['phi'])
+        theta = float(info['theta'])
+
+        phi_raw.append(phi)
+        theta_raw.append(theta)
+
+        x = np.sin(np.deg2rad(theta)) * np.cos(np.deg2rad(phi))
+        y = np.sin(np.deg2rad(theta)) * np.sin(np.deg2rad(phi))
+        z = np.cos(np.deg2rad(theta))
+
+        data_points.append(np.array([x, y, z]))
+
+    return data_points, phi_raw, theta_raw, data_dict_ts
+
+
+def assign_groups_to_TS_LM(data_dict_ts, hsp_lm_groups):
+    """"""
+
+    for structure in data_dict_ts:
+        print(structure)
+
+
+    return
+
+
+# Plotting Functions #
+
+def matplotlib_printing_normal(data_dict, dir, save_status='no', voronoi_status='yes', ts_status='no'):
     # The data from the previous
-    phi_raw        = data_dict['phi_raw']
-    theta_raw      = data_dict['theta_raw']
-    phi_centers    = data_dict['phi_skm_centers']
-    theta_centers  = data_dict['theta_skm_centers']
-    phi_vertices   = data_dict['phi_sv_centers']
-    theta_vertices = data_dict['theta_sv_centers']
+    phi_raw = data_dict['phi_raw']
+    theta_raw = data_dict['theta_raw']
+    phi_centers = data_dict['phi_skm_centers']
+    theta_centers = data_dict['theta_skm_centers']
+    phi_vertices = data_dict['phi_sv_vertices']
+    theta_vertices = data_dict['theta_sv_vertices']
 
     # Canonical Designations
     pucker, phi_cano, theta_cano = read_csv_canonical_designations('CP_params.csv', dir)
-
 
     fig, ax = plt.subplots(facecolor='white')
 
@@ -198,47 +378,59 @@ def matplotlib_printing_normal(data_dict, dir, save_status='no'):
     ax.set_xticks(major_ticksx)
     ax.set_xticks(minor_ticksx, minor=True)
 
-    major_ticksy = np.arange(0,182, 30)
-    minor_ticksy = np.arange(0,182, 10)
+    major_ticksy = np.arange(0, 182, 30)
+    minor_ticksy = np.arange(0, 182, 10)
     ax.set_yticks(major_ticksy)
     ax.set_yticks(minor_ticksy, minor=True)
 
-    ax.set_xlim([-5, 365])
+    ax.set_xlim([-10, 370])
     ax.set_ylim([185, -5])
     ax.set_xlabel('Phi (degrees)')
     ax.set_ylabel('Theta (degrees)')
 
     hsp = ax.scatter(phi_raw, theta_raw, s=60, c='blue', marker='o')
     kmeans = ax.scatter(phi_centers, theta_centers, s=60, c='red', marker='h')
-    voronoi = ax.scatter(phi_vertices, theta_vertices, s=60, c='green', marker='s')
+    if voronoi_status == 'yes':
+        voronoi = ax.scatter(phi_vertices, theta_vertices, s=60, c='green', marker='s')
     cano = ax.scatter(phi_cano, theta_cano, s=60, c='black', marker='+')
 
-    leg = ax.legend((hsp, kmeans, voronoi, cano),
-              ('HSP local minima', 'k-means center (k = ' + str(data_dict['number_clusters']) + ')', 'voronoi vertice',
-               'canonical designation'),
-                scatterpoints=1, fontsize=12, frameon='false')
+
+    if voronoi_status =='yes':
+        leg = ax.legend((hsp, kmeans, voronoi, cano),
+                        ('HSP local minima', 'k-means center (k = ' + str(data_dict['number_clusters']) + ')',
+                         'voronoi vertice',
+                         'canonical designation'),
+                        scatterpoints=1, fontsize=12, frameon='false')
+    else:
+        leg = ax.legend((hsp, kmeans, cano),
+                        ('HSP local minima', 'k-means center (k = ' + str(data_dict['number_clusters']) + ')',
+                         'canonical designation'),
+                        scatterpoints=1, fontsize=12, frameon='false')
 
     leg.get_frame().set_linewidth(0.0)
 
     if save_status != 'no':
-        filename = create_out_fname('bxyl-k' + str(data_dict['number_clusters']) + '-normal.png', base_dir=dir)
-        plt.savefig(filename, facecolor=fig.get_facecolor(), transparent=True)
+        if ts_status == 'no':
+            filename = create_out_fname('bxyl-k' + str(data_dict['number_clusters']) + '-normal.png', base_dir=dir)
+            plt.savefig(filename, facecolor=fig.get_facecolor(), transparent=True)
+        elif ts_status == 'yes':
+            filename = create_out_fname('bxyl-TS-k' + str(data_dict['number_clusters']) + '-normal.png', base_dir=dir)
+            plt.savefig(filename, facecolor=fig.get_facecolor(), transparent=True)
     else:
         plt.show()
 
     return
 
 
-def matplotlib_printing_size(data_dict, dir, save_status='no'):
-
+def matplotlib_printing_size_bxyl_lm(data_dict, dir, save_status='no'):
     # The data from the previous
-    phi_raw        = data_dict['phi_raw']
-    theta_raw      = data_dict['theta_raw']
-    phi_centers    = data_dict['phi_skm_centers']
-    theta_centers  = data_dict['theta_skm_centers']
-    phi_vertices   = data_dict['phi_sv_centers']
-    theta_vertices = data_dict['theta_sv_centers']
-    energy         = data_dict['energy']
+    phi_raw = data_dict['phi_raw']
+    theta_raw = data_dict['theta_raw']
+    phi_centers = data_dict['phi_skm_centers']
+    theta_centers = data_dict['theta_skm_centers']
+    phi_vertices = data_dict['phi_sv_vertices']
+    theta_vertices = data_dict['theta_sv_vertices']
+    energy = data_dict['energy']
 
     # Generating the marker size based on energy
     max_energy = float(max(energy))
@@ -259,12 +451,12 @@ def matplotlib_printing_size(data_dict, dir, save_status='no'):
     ax.set_xticks(major_ticksx)
     ax.set_xticks(minor_ticksx, minor=True)
 
-    major_ticksy = np.arange(0,182, 30)
-    minor_ticksy = np.arange(0,182, 10)
+    major_ticksy = np.arange(0, 182, 30)
+    minor_ticksy = np.arange(0, 182, 10)
     ax.set_yticks(major_ticksy)
     ax.set_yticks(minor_ticksy, minor=True)
 
-    ax.set_xlim([-5, 365])
+    ax.set_xlim([-10, 370])
     ax.set_ylim([185, -5])
     ax.set_xlabel('Phi (degrees)')
     ax.set_ylabel('Theta (degrees)')
@@ -273,9 +465,23 @@ def matplotlib_printing_size(data_dict, dir, save_status='no'):
     kmeans = ax.scatter(phi_centers, theta_centers, s=60, c='red', marker='h')
     cano = ax.scatter(phi_cano, theta_cano, s=60, c='black', marker='+')
 
+    for i, txt in enumerate(pucker):
+        if float(theta_cano[i]) < 120 and float(theta_cano[i]) > 60 and float(phi_cano[i]) < 355:
+            ax.annotate(txt, xy=(phi_cano[i], theta_cano[i]),
+                        xytext=(float(phi_cano[i]) - 7, float(theta_cano[i]) + 12))
 
-    ax.annotate(pucker[25], xy=(phi_cano[25],theta_cano[25]))
-    # ax.annotate(, (,))
+    ax.annotate(str(energy[5]) + r' ${\frac{kcal}{mol}}$', xy=(phi_raw[5], theta_raw[5]),
+                xytext=(float(phi_raw[5]) + 18, float(theta_raw[5]) - 15), arrowprops=dict(arrowstyle="->",
+                                                                                           connectionstyle="arc3"), )
+    ax.annotate(str(energy[20]) + r' ${\frac{kcal}{mol}}$', xy=(phi_raw[20], theta_raw[20]),
+                xytext=(float(phi_raw[20]) - 20, float(theta_raw[20]) - 15), arrowprops=dict(arrowstyle="->",
+                                                                                             connectionstyle="arc3"), )
+    ax.annotate(str(energy[11]) + r' ${\frac{kcal}{mol}}$', xy=(phi_raw[11], theta_raw[11]),
+                xytext=(float(phi_raw[11]) - 20, float(theta_raw[11]) - 15), arrowprops=dict(arrowstyle="->",
+                                                                                             connectionstyle="arc3"), )
+    ax.annotate(str(energy[24]) + r' ${\frac{kcal}{mol}}$', xy=(phi_raw[24], theta_raw[24]),
+                xytext=(float(phi_raw[24]) - 20, float(theta_raw[24]) - 15), arrowprops=dict(arrowstyle="->",
+                                                                                             connectionstyle="arc3"), )
 
     leg = ax.legend((hsp, kmeans),
                     ('HSP local minima', 'k-means center (k = ' + str(data_dict['number_clusters']) + ')',
@@ -290,5 +496,57 @@ def matplotlib_printing_size(data_dict, dir, save_status='no'):
     else:
         plt.show()
 
+    return
+
+
+def matplotlib_printing_group_labels(groups, dir_, save_status='no'):
+
+    phi_values = []
+    theta_values = []
+
+    for key, key_val in groups.items():
+        phi_values.append(key_val['mean_phi'])
+        theta_values.append(key_val['mean_theta'])
+
+    # The plotting for this function is completed below.
+    fig, ax = plt.subplots(facecolor='white')
+
+    major_ticksx = np.arange(0, 372, 60)
+    minor_ticksx = np.arange(0, 372, 12)
+    ax.set_xticks(major_ticksx)
+    ax.set_xticks(minor_ticksx, minor=True)
+
+    major_ticksy = np.arange(0, 182, 30)
+    minor_ticksy = np.arange(0, 182, 10)
+    ax.set_yticks(major_ticksy)
+    ax.set_yticks(minor_ticksy, minor=True)
+
+    ax.set_xlim([-10, 370])
+    ax.set_ylim([185, -5])
+    ax.set_xlabel('Phi (degrees)')
+    ax.set_ylabel('Theta (degrees)')
+
+    # for key, value in group_dict.items():
+    kmeans = ax.scatter(phi_values, theta_values, s=60, c='red', marker='h')
+
+    for key, value in groups.items():
+        if float(value['mean_theta']) < 30:
+            ax.annotate('G-' + key.split('_')[1], xy=(float(value['mean_phi']), float(value['mean_theta'])),
+                        xytext=(float(value['mean_phi']) - 10, float(value['mean_theta']) + 15),
+                        arrowprops=dict(arrowstyle="->",
+                                        connectionstyle="arc3"), )
+        else:
+            ax.annotate('G-' + key.split('_')[1], xy=(float(value['mean_phi']), float(value['mean_theta'])),
+                        xytext=(float(value['mean_phi']) - 10, float(value['mean_theta']) - 15),
+                        arrowprops=dict(arrowstyle="->",
+                                        connectionstyle="arc3"), )
+
+
+
+    if save_status != 'off':
+        filename = create_out_fname('bxyl-k' + str(len(groups)) + '-groups.png', base_dir=dir_)
+        plt.savefig(filename, facecolor=fig.get_facecolor(), transparent=True)
+    else:
+        plt.show()
 
     return
--- tests/test_spherical_kmeans_voronoi.py
+++ tests/test_spherical_kmeans_voronoi.py
@@ -9,11 +9,13 @@ test_xyz_cluster
 import logging
 import os
 import unittest
+import pandas as pd
 
 from qm_utils.qm_common import silent_remove, diff_lines, capture_stderr, capture_stdout, create_out_fname, \
     write_csv, list_to_dict, read_csv_to_dict
 from qm_utils.spherical_kmeans_voronoi import read_csv_data, spherical_kmeans_voronoi, \
-    matplotlib_printing_size, matplotlib_printing_normal, read_csv_canonical_designations
+    matplotlib_printing_size_bxyl_lm, matplotlib_printing_normal, read_csv_canonical_designations, \
+    organizing_information_from_spherical_kmeans, matplotlib_printing_group_labels, read_csv_data_TS
 from qm_utils.xyz_cluster import main, hartree_sum_pucker_cluster, compare_rmsd_xyz, test_clusters, \
     check_ring_ordering, read_ring_atom_ids, check_before_after_sorting
 
@@ -32,6 +34,7 @@ SUB_DATA_DIR = os.path.join(DATA_DIR, 'spherical_kmeans_voronoi')
 
 # Input files #
 HSP_LOCAL_MIN = 'z_lm-b3lyp_howsugarspucker.csv'
+HSP_TRANS_STA = 'z_TS-b3lyp_howsugarspucker.csv'
 CANO_DESIGN   = 'CP_params.csv'
 
 # Good output
@@ -41,6 +44,7 @@ THETA_RAW_GOOD  = [91.8, 177.9, 177.8, 176.6, 177.7, 86.2, 89.4, 89.2, 92.6, 90.
                   88.1, 92.3, 86.3, 86.8, 86.2, 89.6, 89.7, 92.4, 90.8, 92.5]
 NUMBER_CLUSTERS = 7
 SKM_LABELS_GOOD = 'array([4, 2, 2, 2, 2, 3, 4, 4, 4, 4, 0, 3, 1, 1, 5, 5, 0, 0, 1, 1, 3, 1, 1, 6, 6, 6], dtype=int32)'
+HSP_LM_DF_GOOD  = os.path.join(SUB_DATA_DIR, 'a_HSP_lm_reference_groups_good.csv')
 
 # noinspection PyUnboundLocalVariable
 class TestSphereicalKmeansVoronoi(unittest.TestCase):
@@ -58,29 +62,63 @@ class TestSphereicalKmeansVoronoi(unittest.TestCase):
         finally:
             self.assertEqual(data_dict['number_clusters'], NUMBER_CLUSTERS)
 
-
     def testImportCanonicalDesignation(self):
         try:
             pucker, phi_cano, theta_cano = read_csv_canonical_designations(CANO_DESIGN, SUB_DATA_DIR)
         finally:
             self.assertEqual(pucker[0], '1c4')
 
-
     def testMatplotlibPrinting(self):
         data_points, phi_raw, theta_raw, energy = read_csv_data(HSP_LOCAL_MIN, SUB_DATA_DIR)
         data_dict = spherical_kmeans_voronoi(7, data_points, phi_raw, theta_raw, energy)
         matplotlib_printing_normal(data_dict, SUB_DATA_DIR, save_status='no')
-        matplotlib_printing_size(data_dict, SUB_DATA_DIR, save_status='no')
+        matplotlib_printing_size_bxyl_lm(data_dict, SUB_DATA_DIR, save_status='no')
 
+    def testGroupAssignment(self):
+        data_points, phi_raw, theta_raw, energy = read_csv_data(HSP_LOCAL_MIN, SUB_DATA_DIR)
+        data_dict = spherical_kmeans_voronoi(9, data_points, phi_raw, theta_raw, energy)
+        final_groups = organizing_information_from_spherical_kmeans(data_dict)
+        matplotlib_printing_group_labels(final_groups, dir_=SUB_DATA_DIR, save_status='off')
 
 class MainRun(unittest.TestCase):
     def testMainRun(self):
         try:
-            # Input
-            number_clusters = 8
+            # Input Parameters #
+            number_clusters = 9
+            save_status = 'on'
         finally:
-            # Running
+            # Running #
             data_points, phi_raw, theta_raw, energy = read_csv_data(HSP_LOCAL_MIN, SUB_DATA_DIR)
             data_dict = spherical_kmeans_voronoi(number_clusters, data_points, phi_raw, theta_raw, energy)
-            # matplotlib_printing_normal(data_dict, SUB_DATA_DIR, save_status='no')
-            matplotlib_printing_size(data_dict, SUB_DATA_DIR, save_status='no')
+            final_groups = organizing_information_from_spherical_kmeans(data_dict)
+            df = pd.DataFrame.from_dict(final_groups)
+            out_file_name = create_out_fname('a_HSP_lm_reference_groups', base_dir=SUB_DATA_DIR, ext='.csv')
+            df.to_csv(out_file_name)
+
+            # Testing #
+            self.assertFalse(diff_lines(out_file_name, HSP_LM_DF_GOOD))
+            silent_remove(out_file_name)
+
+            # Plotting Commands #
+            matplotlib_printing_normal(data_dict, SUB_DATA_DIR, save_status=save_status)
+            matplotlib_printing_size_bxyl_lm(data_dict, SUB_DATA_DIR, save_status=save_status)
+            matplotlib_printing_group_labels(final_groups, dir_=SUB_DATA_DIR, save_status=save_status)
+            print('\n Done \n ')
+            pass
+
+    def testMainRunTS(self):
+        try:
+            # Input Parameters #
+            number_cluster = 25
+            save_status = 'yes'
+            hsp_lm_dict = pd.read_csv(HSP_LM_DF_GOOD)
+
+        finally:
+            # Running #
+            read_csv_data_TS(HSP_TRANS_STA, SUB_DATA_DIR)
+        #     data_points, phi_raw, theta_raw, energy = read_csv_data(HSP_TRANS_STA, SUB_DATA_DIR)
+        #     data_dict = spherical_kmeans_voronoi(number_clusters, data_points, phi_raw, theta_raw, energy)
+        #     final_ts_groups = organizing_information_from_spherical_kmeans(data_dict)
+        #     matplotlib_printing_normal(data_dict, SUB_DATA_DIR, save_status=save_status, voronoi_status='no', ts_status='yes')
+        #     print('\n Done \n ')
+        #     pass
