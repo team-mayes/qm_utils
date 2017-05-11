99cd8c641e3e00522b01a1d0a606be5406141cd1
Vicchio-7 <vicchio@umich.edu>
2017-05-11 10:38:23 -0400

    about to receive the update form Justin...

--- tests/test_spherical_kmeans_voronoi.py
+++ tests/test_spherical_kmeans_voronoi.py
@@ -316,7 +316,6 @@ class MainRun(unittest.TestCase):
             dict_cano = read_csv_canonical_designations('CP_params.csv', SUB_DATA_DIR)
         finally:
             data = Local_Minima(number_clusters, data_points, dict_cano ,phi_raw, theta_raw, energy)
-            data.cano_points
             data.plot_local_min(directory=storage_spot, save_status=save_status)
             data.plot_group_labels(directory=storage_spot, save_status=save_status)
             data.plot_local_min_sizes(directory=storage_spot, save_status=save_status)
@@ -327,7 +326,17 @@ class MainRun(unittest.TestCase):
         try:
             save_status = False
             storage_spot = TRANS_STA_IMAGES
+            # loading in the local minima information
+            number_clusters = 9
+            data_points, phi_raw, theta_raw, energy = read_csv_data(HSP_LOCAL_MIN, SUB_DATA_DIR)
+            dict_cano = read_csv_canonical_designations('CP_params.csv', SUB_DATA_DIR)
+            data = Local_Minima(number_clusters, data_points, dict_cano, phi_raw, theta_raw, energy)
+
+            data.groups_dict
+
+
             data_points_ts, phi_raw_ts, theta_raw_ts, data_dict_ts = read_csv_data_TS(HSP_TRANS_STA, SUB_DATA_DIR)
+
 
             assigned_lm, hsp_lm_dict, phi_ts_lm, theta_ts_lm = assign_groups_to_TS_LM(data_dict_ts, hsp_lm_dict)
 
